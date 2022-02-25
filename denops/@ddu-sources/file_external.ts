c2330384b487a764df3add17d7ac342dab3bf78e
matsui54 <haru.matu9168@gmail.com>
2022-02-25 18:20:19 +0900

    Use abortable to cancel gathering

--- denops/@ddu-sources/file_external.ts
+++ denops/@ddu-sources/file_external.ts
@@ -1,11 +1,9 @@
-import {
-  BaseSource,
-  Item,
-} from "https://deno.land/x/ddu_vim@v0.12.2/types.ts";
+import { BaseSource, Item } from "https://deno.land/x/ddu_vim@v0.12.2/types.ts";
 import { Denops, fn } from "https://deno.land/x/ddu_vim@v0.12.2/deps.ts";
 import { ActionData } from "https://deno.land/x/ddu_kind_file@v0.2.0/file.ts";
 import { relative, resolve } from "https://deno.land/std@0.125.0/path/mod.ts";
 import { BufReader } from "https://deno.land/std@0.125.0/io/buffer.ts";
+import { abortable } from "https://deno.land/std@0.127.0/async/abortable.ts";
 
 type Params = {
   cmd: string[];
@@ -31,64 +29,75 @@ export class Source extends BaseSource<Params> {
     denops: Denops;
     sourceParams: Params;
   }): ReadableStream<Item<ActionData>[]> {
+    const abortController = new AbortController();
     const { denops, sourceParams } = args;
     return new ReadableStream({
       async start(controller) {
-        const tree = async (root: string) => {
-          let items: Item<ActionData>[] = [];
-          const updateItems = sourceParams.updateItems;
+        let root = await fn.expand(denops, sourceParams.path) as string;
+        if (root == "") {
+          root = await fn.getcwd(denops) as string;
+        }
 
-          try {
-            const proc = Deno.run({
-              cmd: [...sourceParams.cmd, root],
-              stdout: "piped",
-              stderr: "piped",
-              cwd: root,
+        if (!args.sourceParams.cmd.length) {
+          return;
+        }
+
+        // controller.close();
+        let items: Item<ActionData>[] = [];
+        const updateItems = sourceParams.updateItems;
+
+        const proc = Deno.run({
+          cmd: [...sourceParams.cmd, root],
+          stdout: "piped",
+          stderr: "piped",
+          cwd: root,
+        });
+
+        try {
+          for await (
+            const line of abortable(
+              iterLine(proc.stdout),
+              abortController.signal,
+            )
+          ) {
+            const path = line.trim();
+            if (!path.length) continue;
+            const fullPath = resolve(root, path);
+            items.push({
+              word: relative(root, fullPath),
+              action: {
+                path: fullPath,
+              },
             });
-
-            for await (const line of iterLine(proc.stdout)) {
-              const path = line.trim();
-              if (!path.length) continue;
-              const fullPath = resolve(root, path);
-              items.push({
-                word: relative(root, fullPath),
-                action: {
-                  path: fullPath,
-                },
-              });
-              if (items.length >= updateItems) {
-                controller.enqueue(items);
-                items = [];
-              }
+            if (items.length >= updateItems) {
+              controller.enqueue(items);
+              items = [];
             }
-
-            const [status, stderr] = await Promise.all([
-              proc.status(),
-              proc.stderrOutput(),
-            ]);
-            proc.close();
-
-            if (!status.success) {
-              console.error(new TextDecoder().decode(stderr));
-            }
-          } catch (e: unknown) {
+          }
+          if (items.length) {
+            controller.enqueue(items);
+          }
+        } catch (e: unknown) {
+          if (e instanceof DOMException) {
+            proc.kill("SIGTERM");
+          } else {
             console.error(e);
           }
-          return items;
-        };
-
-        let dir = await fn.expand(denops, sourceParams.path) as string;
-        if (dir == "") {
-          dir = await fn.getcwd(denops) as string;
+        } finally {
+          const [status, stderr] = await Promise.all([
+            proc.status(),
+            proc.stderrOutput(),
+          ]);
+          proc.close();
+          if (!status.success) {
+            console.error(new TextDecoder().decode(stderr));
+          }
+          controller.close();
         }
+      },
 
-        if (args.sourceParams.cmd.length > 0) {
-          controller.enqueue(
-            await tree(resolve(dir, dir)),
-          );
-        }
-
-        controller.close();
+      cancel(reason): void {
+        abortController.abort(reason);
       },
     });
   }
