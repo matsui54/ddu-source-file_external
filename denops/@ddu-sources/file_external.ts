7e2f99b6580ade19f6ced3bab9a67bfd0b65bb30
matsui54 <matsui54@users.noreply.github.com>
2022-04-22 00:38:25 +0000

    :package: Update Deno dependencies
    Update dependencies by udd:
    
    /home/runner/work/ddu-source-file_external/ddu-source-file_external/denops/@ddu-sources/file_external.ts
    [1/6] Looking for releases: https://deno.land/x/ddu_vim@v1.5.0/types.ts
    [1/6] Using latest: https://deno.land/x/ddu_vim@v1.5.0/types.ts
    [2/6] Looking for releases: https://deno.land/x/ddu_vim@v1.5.0/deps.ts
    [2/6] Using latest: https://deno.land/x/ddu_vim@v1.5.0/deps.ts
    [3/6] Looking for releases: https://deno.land/x/ddu_kind_file@v0.3.0/file.ts
    [3/6] Using latest: https://deno.land/x/ddu_kind_file@v0.3.0/file.ts
    [4/6] Looking for releases: https://deno.land/std@0.135.0/path/mod.ts
    [4/6] Attempting update: https://deno.land/std@0.135.0/path/mod.ts -> 0.136.0
    [4/6] Update successful: https://deno.land/std@0.135.0/path/mod.ts -> 0.136.0
    [5/6] Looking for releases: https://deno.land/std@0.135.0/io/buffer.ts
    [5/6] Attempting update: https://deno.land/std@0.135.0/io/buffer.ts -> 0.136.0
    [5/6] Update successful: https://deno.land/std@0.135.0/io/buffer.ts -> 0.136.0
    [6/6] Looking for releases: https://deno.land/std@0.135.0/async/mod.ts
    [6/6] Attempting update: https://deno.land/std@0.135.0/async/mod.ts -> 0.136.0
    [6/6] Update successful: https://deno.land/std@0.135.0/async/mod.ts -> 0.136.0
    
    /home/runner/work/ddu-source-file_external/ddu-source-file_external/README.md
    
    Already latest version:
    https://deno.land/x/ddu_vim@v1.5.0/types.ts == v1.5.0
    https://deno.land/x/ddu_vim@v1.5.0/deps.ts == v1.5.0
    https://deno.land/x/ddu_kind_file@v0.3.0/file.ts == v0.3.0
    
    Successfully updated:
    https://deno.land/std@0.135.0/path/mod.ts 0.135.0 -> 0.136.0
    https://deno.land/std@0.135.0/io/buffer.ts 0.135.0 -> 0.136.0
    https://deno.land/std@0.135.0/async/mod.ts 0.135.0 -> 0.136.0
    make[1]: Entering directory '/home/runner/work/ddu-source-file_external/ddu-source-file_external'
    make[1]: Leaving directory '/home/runner/work/ddu-source-file_external/ddu-source-file_external'

--- denops/@ddu-sources/file_external.ts
+++ denops/@ddu-sources/file_external.ts
@@ -1,9 +1,9 @@
 import { BaseSource, Item } from "https://deno.land/x/ddu_vim@v1.5.0/types.ts";
 import { Denops, fn } from "https://deno.land/x/ddu_vim@v1.5.0/deps.ts";
 import { ActionData } from "https://deno.land/x/ddu_kind_file@v0.3.0/file.ts";
-import { relative, resolve } from "https://deno.land/std@0.135.0/path/mod.ts";
-import { BufReader } from "https://deno.land/std@0.135.0/io/buffer.ts";
-import { abortable } from "https://deno.land/std@0.135.0/async/mod.ts";
+import { relative, resolve } from "https://deno.land/std@0.136.0/path/mod.ts";
+import { BufReader } from "https://deno.land/std@0.136.0/io/buffer.ts";
+import { abortable } from "https://deno.land/std@0.136.0/async/mod.ts";
 
 const enqueueSize1st = 1000;
 
